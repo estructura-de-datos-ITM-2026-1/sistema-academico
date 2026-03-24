8567d8d83af1815673805b110889f50590782628
santiagoSuarez219 <santiago8628@gmail.com>
2026-03-24 10:31:40 -0500

    Creando la clase aula

---
+++ src/co/edu/itm/sistemaacademico/models/Aula.java
@@ -0,0 +1,58 @@
+public class Aula {
+    private String codigo;
+    private String ubicacion;
+    private String grupos;
+    private  boolean tieneAireAcondicionado;
+    private boolean esBLearning;
+
+    public Aula(String codigo, String ubicacion, String grupos, boolean tieneAireAcondicionado, boolean esBLearning) {
+        this.codigo = codigo;
+        this.ubicacion = ubicacion;
+        this.grupos = grupos;
+        this.tieneAireAcondicionado = tieneAireAcondicionado;
+        this.esBLearning = esBLearning;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(String grupos) {
+        this.grupos = grupos;
+    }
+
+    public boolean isTieneAireAcondicionado() {
+        return tieneAireAcondicionado;
+    }
+
+    public void setTieneAireAcondicionado(boolean tieneAireAcondicionado) {
+        this.tieneAireAcondicionado = tieneAireAcondicionado;
+    }
+
+    public boolean isEsBLearning() {
+        return esBLearning;
+    }
+
+    public void setEsBLearning(boolean esBLearning) {
+        this.esBLearning = esBLearning;
+    } 
+
+    
+    
+}
